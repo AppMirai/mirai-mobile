48f73907216362e121b4621c70f4790c6c891478
RaihanAhnaf <mz.raihan11@gmail.com>
2022-06-01 13:25:00 +0700

    refactor: refactoring code

--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -13,178 +13,157 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    Widget title() {
-      return Container(
-        margin: EdgeInsets.only(top: 20),
-        child: Text(
-          'Register',
-          style: blackTextStyle.copyWith(
-              fontSize: 24, fontWeight: semiBold, color: kPrimaryColor),
-        ),
-      );
-    }
-
-    Widget imageHero() {
-      return Container(
-        child: Image.asset(
-          'assets/images/register_illustration.png',
-          height: 184,
-          width: 184,
-        ),
-      );
-    }
-
-    Widget inputSection() {
-      Widget nameInput() {
-        return Container(
-          margin: EdgeInsets.only(bottom: 20),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            // Text('Email'),
-            // SizedBox(
-            //   height: 6,
-            // ),
-            TextFormField(
-              cursorColor: kPrimaryColor,
-              decoration: InputDecoration(
-                hintText: 'Full Name',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(color: kPrimaryColor)),
-              ),
-            ),
-          ]),
-        );
-      }
-
-      Widget emailInput() {
-        return Container(
-          margin: EdgeInsets.only(bottom: 20),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            // Text('Email'),
-            // SizedBox(
-            //   height: 6,
-            // ),
-            TextFormField(
-              cursorColor: kPrimaryColor,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(color: kPrimaryColor)),
-              ),
-            ),
-          ]),
-        );
-      }
-
-      Widget passwordInput() {
-        return Container(
-          margin: EdgeInsets.only(bottom: 20),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            // Text('Password'),
-            // SizedBox(
-            //   height: 6,
-            // ),
-            TextFormField(
-                cursorColor: kPrimaryColor,
-                obscureText: visible,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      borderSide: BorderSide(color: kPrimaryColor)),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: kPrimaryColor,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        visible = !visible;
-                      });
-                    },
-                  ),
-                )),
-          ]),
-        );
-      }
-
-      Widget submitButton() {
-        return Container(
-          width: double.infinity,
-          height: 55,
-          child: TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              style: TextButton.styleFrom(
-                  backgroundColor: kPrimaryColor,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0))),
-              child: Text('REGISTER',
-                  style: whiteTextStyle.copyWith(
-                    fontSize: 14,
-                    fontWeight: semiBold,
-                  ))),
-        );
-      }
-
-      Widget toLoginButton() {
-        return Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.only(left: 24, right: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("already have account?"),
-              SizedBox(
-                width: 3,
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text(
+                      'Register',
+                      style: blackTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                          color: kPrimaryColor),
+                    ),
+                  ),
+                  Container(
+                    child: Image.asset(
+                      'assets/images/register_illustration.png',
+                      height: 184,
+                      width: 184,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Text('Email'),
+                          // SizedBox(
+                          //   height: 6,
+                          // ),
+                          TextFormField(
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              hintText: 'Full Name',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  borderSide: BorderSide(color: kPrimaryColor)),
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Text('Email'),
+                          // SizedBox(
+                          //   height: 6,
+                          // ),
+                          TextFormField(
+                            cursorColor: kPrimaryColor,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  borderSide: BorderSide(color: kPrimaryColor)),
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Text('Password'),
+                          // SizedBox(
+                          //   height: 6,
+                          // ),
+                          TextFormField(
+                              cursorColor: kPrimaryColor,
+                              obscureText: visible,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8.0)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                    borderSide:
+                                        BorderSide(color: kPrimaryColor)),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    Icons.remove_red_eye,
+                                    color: kPrimaryColor,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      visible = !visible;
+                                    });
+                                  },
+                                ),
+                              )),
+                        ]),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 55,
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: kPrimaryColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0))),
+                        child: Text('REGISTER',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 14,
+                              fontWeight: semiBold,
+                            ))),
+                  ),
+                ],
               ),
-              TextButton(
-                child: Text('Login',
-                    style: pinkTextStyle.copyWith(
-                      fontSize: 14,
-                      fontWeight: regular,
-                    )),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("already have account?"),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        TextButton(
+                          child: Text('Login',
+                              style: pinkTextStyle.copyWith(
+                                fontSize: 14,
+                                fontWeight: regular,
+                              )),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        );
-      }
-
-      return Container(
-        margin: EdgeInsets.only(top: 20),
-        child: Column(
-          children: [
-            nameInput(),
-            emailInput(),
-            passwordInput(),
-            submitButton(),
-            toLoginButton()
-          ],
-        ),
-      );
-    }
-
-    return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: [
-            title(),
-            imageHero(),
-            inputSection(),
-          ],
         ),
       ),
     );
